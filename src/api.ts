6788b8063ba8a37ea45be9f3f8247cd1e73a1b1c
kou-yoshida <kouyoshida28@gmail.com>
2023-12-31 16:22:06 +0900

    🔧 環境変数周りの調整、確認

--- src/api.ts
+++ src/api.ts
@@ -2,6 +2,9 @@
 
 import express, { Router } from "express";
 import serverless from "serverless-http";
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(process.env.DATABASE_URL || '', process.env.SUPABASE_SERVICE_KEY || '')
 
 const api = express();
 // corsの全許可
@@ -16,7 +19,15 @@ router.get('/', (req, res) => {
   res.write('<h1>Hello from Express.jsasdf!</h1>');
   res.end();
 });
-router.get("/hello", (req, res) => res.send("Hello World!"));
+router.get("/hello", async (req, res) => {
+
+const result = await supabase
+ .from('notes')
+ .insert([
+    { note: 'I need to not forget this' },
+ ]).select('*') ;
+  return res.send("Hello World!");
+});
 
 api.use("/api/", router);
 
