dfe5f399cec025eda5313befe816349550b7e9eb
kou-yoshida <kouyoshida28@gmail.com>
2023-12-31 11:09:26 +0900

    🚧 api調整

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -11,6 +11,11 @@ api.use((req, res, next) => {
 });
 
 const router = Router();
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello from Express.js!</h1>');
+  res.end();
+});
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
 api.use("/api/", router);
