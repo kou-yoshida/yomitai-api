66eb816e07584b5b934714ad786407a061547b3d
kou-yoshida <kouyoshida28@gmail.com>
2023-12-31 11:06:05 +0900

    🚧 api調整

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -4,16 +4,16 @@ import express, { Router } from "express";
 import serverless from "serverless-http";
 
 const api = express();
-
-const router = Router();
-router.get("/hello", (req, res) => res.send("Hello World!"));
-
-api.use("/api/", router);
 // corsの全許可
 api.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
+
+const router = Router();
+router.get("/hello", (req, res) => res.send("Hello World!"));
+
+api.use("/api/", router);
 
 
 export const handler = serverless(api);
