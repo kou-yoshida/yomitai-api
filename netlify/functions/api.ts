9c9d873e53e7736e165f00d4e246c716f3dd8e92
kou-yoshida <kouyoshida28@gmail.com>
2023-12-31 10:49:50 +0900

    🚧 api調整

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -6,13 +6,7 @@ import serverless from "serverless-http";
 const api = express();
 
 const router = Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
-});
 router.get("/hello", (req, res) => res.send("Hello World!"));
-router.get("/test", (req, res) => res.send("Test!"));
 
 api.use("/api/", router);
 
