39afbb4bb942a3034700c1ddf76f6a702f225596
kou-yoshida <kouyoshida28@gmail.com>
2023-12-31 15:28:15 +0900

    🚧 api調整

--- src/api.ts
+++ src/api.ts
@@ -13,7 +13,7 @@ api.use((req, res, next) => {
 const router = Router();
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
+  res.write('<h1>Hello from Express.jsasdf!</h1>');
   res.end();
 });
 router.get("/hello", (req, res) => res.send("Hello World!"));
